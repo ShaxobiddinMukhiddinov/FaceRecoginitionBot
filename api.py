549943b051dec3f3ce09107f16d366ee65d520aa
ShaxobidinMuhiddinov <shaxobiddinmuhiddinov429@gmial.com>
2022-05-26 10:19:55 +0500

    api.fix
    Signed-off-by: ShaxobidinMuhiddinov <shaxobiddinmuhiddinov429@gmial.com>

--- api.py
+++ api.py
@@ -5,13 +5,13 @@ def start_command(update, context):
     print(context.bot)
     print(update.message.from_user.id)
     update.message.reply_text(text='Siz /start berdingiz!')
-    # context.bot.send_message(chat_id='103587268', text='Ikkinchi xabar!')
+    context.bot.send_message(chat_id='103587268', text='Ikkinchi xabar!')
 
 
-updater = Updater(token="1954972158:AAGksFgEIsJWyeo2yM6YUjVfQgnS4DQvhZE/")
+updater = Updater(token="1954972158:AAGksFgEIsJWyeo2yM6YUjVfQgnS4DQvhZE")
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start_command))
 
-updater._start_polling()
+updater.start_polling()
 updater.idle()
 
