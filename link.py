cd1f0c44e6b2fd28e82d18b0da66d8bb778a1bd1
ShaxobidinMuhiddinov <shaxobiddinmuhiddinov429@gmial.com>
2022-05-27 07:52:05 +0500

    bot start
    Signed-off-by: ShaxobidinMuhiddinov <shaxobiddinmuhiddinov429@gmial.com>

--- link.py
+++ link.py
@@ -1,11 +1,9 @@
 from telegram.ext import Updater, CommandHandler
 
+
 def start_command(update, context):
-    print(update.message.text)
-    print(context.bot)
-    print(update.message.from_user.id)
-    update.message.reply_text(text='Siz /start berdingiz!')
-    context.bot.send_message(chat_id='103587268', text='Ikkinchi xabar!')
+    # update.message.reply_text(text='Siz /start berdingiz!')
+    context.bot.send_message(chat_id='1035687268', text='Ikkinchi xabar /start!')
 
 
 updater = Updater(token="1954972158:AAGksFgEIsJWyeo2yM6YUjVfQgnS4DQvhZE")
